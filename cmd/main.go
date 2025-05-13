52f1b899bb0fdf285200981f203e92d2ce92437c
Korbinian Kuhn <korbiniankuhn@posteo.de>
2025-05-13 14:27:39 +0200

    ✨ Refactor main function to improve error handling

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/korbiniankuhn/gitops-compose/internal/config"
@@ -13,141 +16,137 @@ import (
 	"github.com/korbiniankuhn/gitops-compose/internal/metrics"
 )
 
+func panicOnError(message string, err error) {
+	if err != nil {
+		slog.Error(message, "error", err)
+		panic(err)
+	}
+}
+
 func main() {
-    slog.Info("starting gitops compose")
+	slog.Info("starting gitops compose")
 
-    // Load config
-    config, err := config.Get()
-    if err != nil {
-        slog.Error("failed to load config", "error", err)
-        panic(err)
-    }
-    slog.Info("config loaded")
+	// Load config
+	config, err := config.Get()
+	panicOnError("failed to load config", err)
+	slog.Info("config loaded")
 
-    // Get credentials from repository if not set
-    if config.RepositoryUsername == "" || config.RepositoryPassword == "" {
-        repoUsername, repoPassword := git.GetCredentialsFromRepository(config.RepositoryPath)
+	// Get credentials from repository if not set
+	if config.RepositoryUsername == "" || config.RepositoryPassword == "" {
+		repoUsername, repoPassword := git.GetCredentialsFromRepository(config.RepositoryPath)
 
-        if repoUsername == "" || repoPassword == "" {
-            slog.Warn("no credentials set in environment or repository origin")
-        } else {
-            slog.Info("credentials set from repository origin")
-            config.SetCredentials(repoUsername, repoPassword)
-        }
-    }
+		if repoUsername == "" || repoPassword == "" {
+			slog.Warn("no credentials set in environment or repository origin")
+		} else {
+			slog.Info("credentials set from repository origin")
+			config.SetCredentials(repoUsername, repoPassword)
+		}
+	}
 
-    // Verify git repository
-    r, err := git.NewDeploymentRepo(config.RepositoryUsername, config.RepositoryPassword, config.RepositoryPath)
-    if err != nil {
-        slog.Error("failed to open deployment repo", "error", err)
-        panic(err)
-    }
+	// Verify git repository
+	r, err := git.NewDeploymentRepo(config.RepositoryUsername, config.RepositoryPassword, config.RepositoryPath)
+	panicOnError("failed to create deployment repo", err)
 
-    // Verify git remote access
-    if err := r.VerifyRemoteAccess(); err != nil {
-        slog.Error("failed to verify git remote access", "error", err)
-        panic(err)
-    }
-    slog.Info("git remote access verified")
+	// Verify git remote access
+	panicOnError("failed to verify git remote access", r.VerifyRemoteAccess())
+	slog.Info("git remote access verified")
 
-    // TODO: skip when go-git is able to pull changes without losing untracked files
-    // Verify git cli
-    if err := r.VerifyGitCli(); err != nil {
-        slog.Error("failed to verify git cli", "error", err)
-        panic(err)
-    }
+	// TODO: skip when go-git is able to pull changes without losing untracked files
+	// Verify git cli
+	panicOnError("failed to verify git cli", r.VerifyGitCli())
 
-    slog.Info("deployment repo configured", "path", config.RepositoryPath)
+	slog.Info("deployment repo configured", "path", config.RepositoryPath)
 
-    // Verify docker socket connection
-    d := docker.NewDocker(config.DockerRegistryUrl, config.DockerRegistryUsername, config.DockerRegistryPassword)
-    if err := d.VerifySocketConnection(); err != nil {
-        slog.Error("failed to verify docker socket connection", "error", err)
-        panic(err)
-    }
-    slog.Info("docker socket connection verified")
+	// Verify docker socket connection
+	d := docker.NewDocker(config.DockerRegistryUrl, config.DockerRegistryUsername, config.DockerRegistryPassword)
+	panicOnError("failed to verify docker socket connection", d.VerifySocketConnection())
+	slog.Info("docker socket connection verified")
 
-    // Verify docker credentials (if set)
-    loggedIn, err := d.LoginIfCredentialsSet()
-    if err != nil {
-        slog.Error("failed to verify docker credentials", "error", err)
-        panic(err)
-    }
-    if loggedIn {
-        slog.Info("docker credentials verified", "url", config.DockerRegistryUrl)
-    }
+	// Verify docker credentials (if set)
+	loggedIn, err := d.LoginIfCredentialsSet()
+	panicOnError("failed to verify docker credentials", err)
+	if loggedIn {
+		slog.Info("docker credentials verified", "url", config.DockerRegistryUrl)
+	}
 
-    // Initialise metrics
-    m := metrics.NewMetrics()
-    if !config.DisableMetrics {
-        http.Handle("/metrics", m.GetMetricsHandler())
-        slog.Info("metrics enabled", "url", "/metrics")
-    } else {
-        slog.Info("skipping metrics (disabled in config)")
-    }
+	// Initialise metrics
+	m := metrics.NewMetrics()
+	if !config.DisableMetrics {
+		http.Handle("/metrics", m.GetMetricsHandler())
+		slog.Info("metrics enabled", "url", "/metrics")
+	} else {
+		slog.Info("skipping metrics (disabled in config)")
+	}
 
-    // Initialise gitops
-    g := gitops.NewGitOps(r, d, m)
-    
-    if err := g.EnsureDeploymentsAreRunning(); err != nil {
-        m.TrackCheckStatus("error")
-        slog.Error("failed to ensure deployments are running on initial start", "error", err)
-    } else {
-        m.TrackCheckStatus("success")
-    }
+	// Initialise gitops
+	g := gitops.NewGitOps(r, d, m)
 
-    trigger := make(chan struct{})
+	if err := g.EnsureDeploymentsAreRunning(); err != nil {
+		m.TrackCheckStatus("error")
+		slog.Error("failed to ensure deployments are running on initial start", "error", err)
+	} else {
+		m.TrackCheckStatus("success")
+	}
 
-    // Run gitops check on trigger
-    go func() {
-        for range trigger {
-            if err := g.CheckAndUpdateDeployments(); err != nil {
-                m.TrackCheckStatus("error")
-            } else {
-                m.TrackCheckStatus("success")
-            }
-        }
-    }()
+	trigger := make(chan struct{})
 
-    // Run gitops check on interval
-    if config.CheckIntervalInSeconds > 0 {
-        slog.Info(fmt.Sprintf("starting gitops repeated pull (every %s seconds)", fmt.Sprint(config.CheckIntervalInSeconds)))
-        go func() {
-            ticker := time.NewTicker(time.Duration(config.CheckIntervalInSeconds) * time.Second)
-            defer ticker.Stop()
-            for range ticker.C {
-                trigger <- struct{}{}
-            }
-        }()
-    } else {
-        slog.Info("skipping gitops repeated pull (check interval is negative)")
-    }
+	// Run gitops check on trigger
+	go func() {
+		for range trigger {
+			if err := g.CheckAndUpdateDeployments(); err != nil {
+				m.TrackCheckStatus("error")
+			} else {
+				m.TrackCheckStatus("success")
+			}
+		}
+		// TODO: graceful shutdown wait group
+	}()
 
-    // Webhook to trigger deployments
-    if !config.DisableWebhook {
-        http.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
-            select {
-                case trigger <- struct{}{}:
-                    slog.Info("triggered check via webhook")
-                default:
-                    slog.Info("ignored webhook as channel is already full")
-            }
-            w.WriteHeader(http.StatusAccepted)
-        })
-        slog.Info("webhook enabled", "url", "/webhook")
-    } else {
-        slog.Info("skipping webhook (disabled in config)")
-    }
-    
-    // Start http server
-    if !config.DisableMetrics || !config.DisableWebhook {
-        if err := http.ListenAndServe(":2112", nil); err != nil {
-            slog.Error("failed to start metrics server", "error", err)
-            panic(err)
-        }
+	// Run gitops check on interval
+	if config.CheckIntervalInSeconds > 0 {
+		slog.Info(fmt.Sprintf("starting gitops repeated pull (every %s seconds)", fmt.Sprint(config.CheckIntervalInSeconds)))
+		go func() {
+			ticker := time.NewTicker(time.Duration(config.CheckIntervalInSeconds) * time.Second)
+			defer ticker.Stop()
+			for range ticker.C {
+				trigger <- struct{}{}
+			}
+		}()
+	} else {
+		slog.Info("skipping gitops repeated pull (check interval is negative)")
+	}
 
-        slog.Info("starting http server", "port", "2112")
-    } else {
-        slog.Info("skipping http server")
-    }
+	// Webhook to trigger deployments
+	if !config.DisableWebhook {
+		http.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
+			select {
+			case trigger <- struct{}{}:
+				slog.Info("triggered check via webhook")
+			default:
+				slog.Info("ignored webhook as channel is already full")
+			}
+			w.WriteHeader(http.StatusAccepted)
+		})
+		slog.Info("webhook enabled", "url", "/webhook")
+	} else {
+		slog.Info("skipping webhook (disabled in config)")
+	}
+
+	// TODO: Move to go routine
+	// Start http server
+	if !config.DisableMetrics || !config.DisableWebhook {
+		panicOnError("failed to start http server", http.ListenAndServe(":2112", nil))
+		slog.Info("starting http server", "port", "2112")
+	} else {
+		slog.Info("skipping http server")
+	}
+
+	// Wait for termination signal
+	osSignal := make(chan os.Signal, 1)
+	signal.Notify(osSignal, syscall.SIGINT, syscall.SIGTERM)
+	<-osSignal
+	slog.Info("received termination signal, shutting down")
+	close(trigger)
+
+	// TODO: graceful shutdown wait group
 }
